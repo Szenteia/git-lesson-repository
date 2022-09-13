6c56435efb0c6e994d4d35ecb98419eb361a6c13
Szentei András <szenteia@gmail.com>
2022-09-13 22:28:07 +0200

    var_let_const

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+
+
+/*console.log(x);
+var x=100;
+console.log(x);
+let x= 100;
+1.példa*/
+
+    for (var i = 0; i < 100; i++) {
+    
+        }
+        var x = mouseX + 100;
+        if (x > 50) {
+        var y = 20;
+        ellipse(x, y, 100, 100);
+        }
+    console.log (i);
+
+
+
+}
